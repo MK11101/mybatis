40a9c4ab89e38309a39be843b1d2f972c8c13363
MK11101 <205726456@qq.com>
2022-08-12 22:34:26 +0800

    基于xml形式的mybatis学习

---
+++ src/main/java/com/MK/Dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.MK.Dao;
+
+import com.MK.domain.Order;
+import com.MK.domain.User;
+
+import java.util.Date;
+import java.util.List;
+
+public interface UserMapper {
+    public List<User> findAll();
+    public User findByid(int id);
+    public List<User> findByids(List list);
+    public User findByCondition(User user);
+
+    public void savebirthday(User user);
+    public User selectbirthday(int id);
+    public List<User> findorder();
+}
